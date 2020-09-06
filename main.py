e4cacbd4498167d253dfe0dad5ee8543c35d11b2
Marcos Antonio Cordovil de Oliveira <marcoantonio.caco@gmail.com>
2020-09-06 13:26:22 -0300

    Utilizadas algumas entadas

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import yfinance as yf
 import utilities
+import os
 
 if __name__ == '__main__':
+    os.environ["DEBUG"] = "True"
+
     sector_list = list(["Energy", "Technology"])
 
-    utilities.get_all_stocks_by_sector_list(sector_list)
+    energy_stocks = utilities.get_all_stocks_by_sector("Energy")
+    p_stocks_list = list()
+    ticker_list = list()
+
+    for s in energy_stocks:
+        ticker = yf.Ticker(s)
+        try:
+            if len(ticker_list) < 10:
+                print(f"Informaçoes da  açao {s}:\n{ticker.info}")
+                ticker_list.append(ticker)
+            else:
+                break
+        except IndexError as e:
+            print(f"Algum problema com a açao {s}")
+            # print(e)
+            p_stocks_list.append(s)
+
+    print("Stocks problematicas:")
+    print(p_stocks_list)
+
+    for t in ticker_list:
+        print(t.actions)
+
+    t = yf.Ticker("AT")
+    print(t.history(period="max"))
+
